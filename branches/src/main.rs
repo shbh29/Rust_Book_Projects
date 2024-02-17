22ff19d570dc956109374fe7803dec4b70667aad
Shubham Singh <shbh29@gmail.com>
2024-02-17 17:52:02 +0530

    else if statement with dynamic values

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,6 +1,26 @@
+use std::io;
+
 fn main() {
-    //assigning values of if
-    let a = 6;
-    let b =  if a < 6 { 5 } else { 10 };
-    println!("value of b is {b}");
+
+    println!("Enter a number");
+
+    let mut number = String::new();
+
+    io::stdin()
+        .read_line(&mut number)
+        .expect("Could not get user input");
+
+    let number: i32 = number.trim().parse().expect("Please enter a valid number");
+
+    if number % 4 == 0 {
+        println!("Number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("Number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("Number is divisible by 2");
+    } else {
+        println!("Number is not divisible by 4, 3 and 2.");
+    }
+
+
 }
