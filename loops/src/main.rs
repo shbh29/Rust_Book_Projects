e0f4ade047f54782af092aac55eebba90169ac4b
Shubham Singh <shbh29@gmail.com>
2024-02-17 18:07:31 +0530

    Loop with label and break label

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,5 +1,14 @@
 fn main() {
-    loop {
+    let mut counter = 0;
+    'label1:loop {
+        let mut y = 10;
+        loop {
+            println!("remaining: {y}");
+            if y == 9 { break; }
+            if counter > 5 { break 'label1; }
+            y -= 1;
+        }
         println!("Hello, world!");
+        counter += 1;
     }
 }
