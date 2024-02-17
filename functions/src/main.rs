a1b9b4d549a625ab0183e292b8ac0a55904a5300
Shubham Singh <shbh29@gmail.com>
2024-02-17 14:17:48 +0530

    with parameters

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    let x = calculate_x();
+    let x = calculate_x(55);
 
     println!("value of x is {x}")
 }
 
-fn calculate_x() -> i32 {
-    7
+fn calculate_x(init_value: i32) -> i32 {
+    init_value + 1
 }
