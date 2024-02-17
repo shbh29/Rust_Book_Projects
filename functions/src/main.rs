267758852d8e07e62a78eb06cbbf53f9eb90a255
Shubham Singh <shbh29@gmail.com>
2024-02-17 14:25:45 +0530

    multiple argument of a function. and comments

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,17 @@
 fn main() {
-    let x = calculate_x(55);
+    let x = calculate_x(55, 76);
 
-    println!("value of x is {x}")
+    println!("value of x is {x}");
+    
+    // Rust does not support function overloading, instead Traits are used.
+    //let x = calculate_x(55);
+
+    //println!("value of x is {x}");
 }
 
-fn calculate_x(init_value: i32) -> i32 {
-    init_value + 1
+fn calculate_x(init_value: i32, add_value: i32) -> i32 {
+    init_value + add_value
 }
+//fn calculate_x(init_value: i32) -> i32 {
+//    init_value + 1
+//}
