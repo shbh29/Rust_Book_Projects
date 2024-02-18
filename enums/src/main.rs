f46f2ae1046845bd58fcad2ee44dda90a2b3e94d
Shubham Singh <shbh29@gmail.com>
2024-02-19 02:37:24 +0530

    Adding differenct constructors to IpAddrKind enum

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,14 +1,14 @@
 
 #[derive(Debug)]
 enum IpAddrKind {
-    V4,
-    V6,
+    V4(u8,u8,u8,u8),
+    V6(String),
 }
 
 fn main() {
-   let home = IpAddrKind::V4;
+   let home = IpAddrKind::V4(127,0,0,0);
 
-   let localhost  = IpAddrKind::V6;
+   let localhost  = IpAddrKind::V6(String::from("::1"));
 
    dbg!(&home);
    dbg!(&localhost);
