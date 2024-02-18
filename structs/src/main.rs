5783047c04c16197d6e9d365ca9eefede09cb60e
Shubham Singh <shbh29@gmail.com>
2024-02-18 17:13:52 +0530

    copying values from the object of struct

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -18,4 +18,31 @@ fn main() {
     println!("email of User is {}!", user.email);
     println!("Age of User is {}!", user.age);
     println!("Status of User is {}!", user.active);
+
+    // Copy the user 1 to user to changing the email
+    let user2 = User {
+        email: String::from("mst.shubhamsingh@gmail.com"),
+        ..user
+    };
+
+    println!("email of User 2 is {}!", user2.email);
+
+    // able to access user after copying values to user2?
+    // println!("Name of User is {}!", user.name);
+    // value is not accessible
+    //
+
+    // But, value should be accessible is all the Heap memory members are overwritten.
+    let user3 = User {
+        name: String::from("John"),
+        email: String::from("joh.doe@email.com"),
+        ..user2
+    };
+
+
+    println!("email of User 3 is {}!", user3.email);
+    println!("email of User 2 is {}!", user2.email);
+
+
+
 }
