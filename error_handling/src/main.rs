5eb7575eac50f58f701410a9c4b2a074ae3e538a
Shubham Singh <shbh29@gmail.com>
2024-02-22 11:23:47 +0530

    main method refactored

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,7 +1,9 @@
+use error_handling::{error_handle, matching_for_errors, unwrap_for_errors};
+
 
 
 fn main() {
-   error_handling::matching_for_errors::file_operation_with_match(); 
-   error_handling::unwrap_for_errors::unwrap_method_for_file_opening(); 
-   error_handling::error_handle::handle_error();
+   matching_for_errors::file_operation_with_match(); 
+   unwrap_for_errors::unwrap_method_for_file_opening(); 
+   error_handle::handle_error();
 }
