85038ee13735311162158a2f3c4a5f87dbea6b39
Shubham Singh <shbh29@gmail.com>
2024-02-25 19:11:14 +0530

    print the error to standard error.

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,12 +7,12 @@ fn main () {
     let arguments_vector : Vec<String> = env::args().collect();
 
     let config = Config::build(&arguments_vector).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(message) = minigrep::run(&config) {
-        println!("Application Error: {}", message);
+        eprintln!("Application Error: {}", message);
         process::exit(1);
     }
 }
