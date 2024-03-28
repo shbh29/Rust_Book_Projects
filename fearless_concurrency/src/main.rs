07874d142c9a21c777ab6c48d8e08de7738119f9
Shubham Singh <shbh29@gmail.com>
2024-03-28 15:18:19 +0530

    multi threading chal rahi hai. Simple Not so impressing multhreading counter with Arc and Mutex.

--- fearless_concurrency/src/main.rs
+++ fearless_concurrency/src/main.rs
@@ -1,5 +1,24 @@
+use std::{sync::{Arc, Mutex}, thread};
+
 
 
 fn main() {
-    
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let m = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            println!(" mutex: {:?} ", m);
+            let mut val = m.lock().unwrap();
+            *val += 1;
+        });
+        handles.push(handle);
+    }
+
+    for h in handles {
+        h.join().unwrap();
+    }
+
+    println!("mutex: {:?} ", counter.lock().unwrap());
 }
