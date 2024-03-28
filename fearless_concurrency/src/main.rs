621c4c387af742479a5844c7189de745cf05be4d
Shubham Singh <shbh29@gmail.com>
2024-03-28 14:25:14 +0530

    mutex and multiple tx moved to module for a channel

--- fearless_concurrency/src/main.rs
+++ fearless_concurrency/src/main.rs
@@ -1,44 +1,5 @@
-use std::{sync::mpsc, thread, time::Duration};
 
 
 fn main() {
-    let (tx, rx) = mpsc::channel();
-
-    let tx1 = tx.clone();
-
-    thread::spawn(move || {
-        let v = vec![
-            String::from("hi"),
-            String::from("hello"),
-            String::from("Namaste"),
-            String::from("Ram Ram")
-        ];
-        
-        for m in v {
-            tx.send(m).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-        
-    });
-
-
-    thread::spawn(move || {
-        let v = vec![
-            String::from("More"),
-            String::from("Messages"),
-            String::from("for"),
-            String::from("You!")
-        ];
-        
-        for m in v {
-            tx1.send(m).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-        
-    });
-
-    for r in rx {
-        println!("Got: {}", r);
-    }
     
 }
